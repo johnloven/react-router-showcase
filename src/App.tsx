2817798a29b91ab8022fc95fec25950e6137427d
John Lovén <john.loven@lmx.nu>
2024-10-11 13:50:21 +0200

    React router example

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,62 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Route, Routes } from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <input
+        id="loggedInCheckbox"
+        type="checkbox"
+        onChange={(e) => setLoggedIn(e.target.checked)}
+      />
+      <label htmlFor="loggedInCheckbox">Logged in: {String(loggedIn)}</label>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">/</Link>
+          </li>
+          <li>
+            <Link to="/login">/login</Link>
+          </li>
+          <li>
+            <Link to="/public">/public</Link>
+          </li>
+          <li>
+            <Link to="/authenticated">/authenticated</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route element="Everybody can see this page" path="/public" />
+        <Route
+          path="*"
+          element={
+            loggedIn ? (
+              <Routes>
+                <Route path="/" element="You are logged in" />
+                <Route
+                  path="/authenticated"
+                  element="You can see this page because you are logged in"
+                />
+                <Route path="*" element="Page not found" />
+              </Routes>
+            ) : (
+              <Routes>
+                <Route path="/" element="You are not logged in" />
+                <Route
+                  path="/login"
+                  element="You can see this page because you are not logged in"
+                />
+                <Route path="*" element="Page not found" />
+              </Routes>
+            )
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
